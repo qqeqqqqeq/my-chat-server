0a28009c5cc86b7ab9a8c19dd37322d0f2d8904e
Shen <987570929@qq.com>
2017-08-03 09:37:23 +0800

    chat server

--- client.cpp
+++ client.cpp
@@ -12,6 +12,11 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+int comm_fd;
 
 void error(const char *msg)
 {
@@ -19,51 +24,129 @@ void error(const char *msg)
     exit(0);
 }
 
-int main(int argc, char *argv[])
+void *read(void* ptr)
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
+	int nfds,n=0;
+    fd_set fds;
     char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
-    while(1)
+    int retval;
+    while (1)
     {
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0)
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0)
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+		FD_ZERO(&fds);
+		FD_SET(comm_fd,&fds);
+		nfds=comm_fd+1;
+
+		retval = select(nfds,&fds,NULL,NULL,NULL);
+		if( -1 == retval )
+		{
+			printf("select error\n");
+			break;
+		}
+
+		else if( 0 == retval )
+		{
+			printf("Timeout \n");
+		}
+		else
+		{
+			bzero(buffer, sizeof(buffer));
+			n = read(comm_fd, buffer, sizeof(buffer));
+			if (n < 0)
+				error("ERROR reading from socket");
+			else if (n>0)
+			{
+				printf("Receive a message :%s \n",buffer);
+			}
+		}
     }
-    close(sockfd);
+
     return 0;
 }
 
+
+void *write(void* ptr)
+{
+
+	int n=0;
+	char buffer[256];
+
+	while(1)
+	{
+		printf("Please enter the message: ");
+		bzero(buffer,256);
+		fgets(buffer,255,stdin);
+		n = write(comm_fd,buffer,strlen(buffer));
+		if (n < 0)
+			error("ERROR writing to socket");
+	}
+}
+
+
+
+void check_port(int a,char *b[])
+{
+    if (a < 3)
+    {
+       fprintf(stderr,"usage %s host name port\n", b[0]);
+       exit(0);
+    }
+}
+
+void check_fd(int a)
+{
+    if (a < 0)
+        error("ERROR opening socket");
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno,ret;
+
+    pthread_t pid[2];
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    check_port(argc,argv);
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    check_fd(sockfd);
+
+
+    server = gethostbyname(argv[1]);
+    if (server == NULL)
+    {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+
+    bcopy((char *)server->h_addr,
+    		(char *)&serv_addr.sin_addr.s_addr,
+			server->h_length);
+
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,
+    		sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+    comm_fd=sockfd;
+    ret=pthread_create(&pid[1],NULL,read,NULL);
+    if(ret!=0)
+    	printf("Create read thread error!\n");
+
+    ret=pthread_create(&pid[2],NULL,write,NULL);
+    if(ret!=0)
+    	printf("Create write thread error!\n");
+
+    while(1)
+    {
+
+    }
+    return 0;
+}
 
 
--- server.cpp
+++ server.cpp
@@ -9,12 +9,26 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define thr_num 5
+#define THR_NUM 5
+
+struct thread_info{
+	int thr_fd;
+	bool thr_ck;
+};
+struct th_ep{
+	int num;
+	int thread_epfd;
+}ep;
+struct epoll_event ev,events[20];
 
 
-int thread_fd[thr_num]={-1,-1,-1,-1,-1};
-bool thread_ck[thr_num]={true,true,true,true,true};
-sem_t sem_id[thr_num];
+thread_info THREAD_INFO[THR_NUM]={-1,true};
+sem_t sem_id[THR_NUM];
+int fd_list[10]={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+
+
+
+
 
 
 void error(const char *msg)
@@ -23,9 +37,9 @@ void error(const char *msg)
     exit(1);
 }
 
-void creat_sem()
+void create_sem()
 {
-    for(int i=1;i<3;i++)
+    for(int i=0;i<THR_NUM;i++)
     {
     	int res = sem_init(&sem_id[i], 0, 0);
 		if(res == -1)
@@ -38,49 +52,94 @@ void creat_sem()
 
 void *thread(void* ptr)
 {
-	int fd=-1,n,thread_num;
-	char buffer[256];
-	thread_num=*(int *)ptr;
-	printf("thread %d created \n",thread_num);
+	pthread_detach(pthread_self());
+	int fd =-1,n,thread_no,epfd;
+	char buffer[256],cli_id[256],offline_message[50];
+
+	th_ep *thr=(th_ep *)ptr;
+	thread_no=thr->num;
+	epfd=thr->thread_epfd;
+	printf("thread %d created \n",thread_no);
+
+	THREAD_INFO[thread_no].thr_fd=1;
+
 	while(1)
 	{
-		sem_wait(&sem_id[thread_num]);
-		thread_ck[thread_num]=false;
-		if(thread_fd[thread_num]!=-1)
+		sem_wait(&sem_id[thread_no]);
+		if(THREAD_INFO[thread_no].thr_fd!=-1)
 		{
+			fd=THREAD_INFO[thread_no].thr_fd;
+			bzero(buffer,256);
+			n=read(fd,buffer,256);
 
-		fd=thread_fd[thread_num];
-		bzero(buffer,256);
-		n=read(fd,buffer,256);
 
-		if (n==0)
-		{
-			close(fd);
-			printf("Client %d is off line\n",fd-4);
-			thread_fd[thread_num]=-1;
-			thread_ck[thread_num]=true;
-			continue;
+			if (n==0)
+			{
+				ev.data.fd=fd;
+				ev.events=EPOLLIN| EPOLLONESHOT;
+				epoll_ctl(epfd,EPOLL_CTL_DEL,fd,&ev);
+				for (int i=0;i<10;i++)
+				{
+					if(fd_list[i]==fd)
+					{
+						fd_list[i]=-1;
+						close(fd);
+						printf("Socket ID: %d is off line\n",fd);
+						sprintf(offline_message,"\nUser ID: %d offline\n",fd);
+						for (int a=0;a<10;a++)
+						{
+							if(fd_list[a]!=-1)
+							{
+								write(fd_list[a],offline_message,50);
+							}
+						}
+						break;
+					}
+				}
+				THREAD_INFO[thread_no].thr_fd=-1;
+				THREAD_INFO[thread_no].thr_ck=true;
+				continue;
+			}
+			ev.data.fd=fd;
+			ev.events=EPOLLIN| EPOLLONESHOT;
+			epoll_ctl(epfd,EPOLL_CTL_MOD,fd,&ev);
+			printf("Message from Socket ID: %d\n",fd);
+			printf("Here is the message: %s\n",buffer);
+			sprintf(cli_id, "\nMessage from user ID: %d \n",fd);
+			strcat(cli_id, buffer);
+			for ( int i=0;i<10;i++)
+			{
+				if(fd_list[i]!=-1)
+				{
+					if(fd_list[i]!=fd)
+					{
+
+						write(fd_list[i],cli_id,256);
+					}
+				}
+			}
+			THREAD_INFO[thread_no].thr_ck=true;
 		}
+		else
+		{
+			printf("Socket ID error \n");
 
-		printf("Message from : %d\n",fd-4);
-		printf("Here is the message: %s\n",buffer);
-		write(fd,"Message received",17);
-		thread_ck[thread_num]=true;
 		}
 	}
 	return 0;
 }
 
-void creat_thread(pthread_t pid[1])
+void create_thread(pthread_t pid[],int ep_fd)
 {
-	int ret,b=0;
-    for(int a=0;a<thr_num;a++)
+	int ret;
+    for(int a=0;a<THR_NUM;a++)
     {
-    	b=a+1;
-    	ret=pthread_create(&pid[a],NULL,thread,&b);
-    	sleep(0.1);
+    	ep.num=a;
+    	ep.thread_epfd=ep_fd;
+    	ret=pthread_create(&pid[a],NULL,thread,&ep);
+    	sleep(0.05);
     	if(ret!=0)
-    		printf("Create thread %d error!\n",b);
+    		printf("Create thread %d error!\n",a);
     }
 }
 
@@ -93,40 +152,25 @@ void check_port(int portno)
 	    }
 }
 
-void accept_event(epoll_event ev,epoll_event events[20],int lis_fd,int epfd )
-{
-	if(lis_fd<0)
-	{
-		error("accept fail");
-	}
-	ev.data.fd=lis_fd;
-	ev.events=EPOLLIN;
-	epoll_ctl(epfd,EPOLL_CTL_ADD,lis_fd,&ev);
-
-}
-
 
 int main(int argc, char *argv[])
 {
 
 	int portno,epfd,nfds,new_fd, listenfd;
-	struct epoll_event ev,events[20];
+
 	struct sockaddr_in serv_addr, cli_addr;
     socklen_t clilen;
-    pthread_t pid[1];
-
+    pthread_t pid[THR_NUM];
 
     check_port(argc);
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     if (listenfd == -1)
     	error("ERROR opening socket");
 
-    creat_sem();
-
-    creat_thread(pid);
-
+    create_sem();
 
  	epfd=epoll_create(20);
+ 	create_thread(pid,epfd);
  	ev.data.fd=listenfd;
  	ev.events=EPOLLIN;
  	epoll_ctl(epfd,EPOLL_CTL_ADD,listenfd,&ev);
@@ -145,28 +189,39 @@ int main(int argc, char *argv[])
 
     while(1)
     {
-    	nfds=epoll_wait(epfd,events,20,-1);
+    	nfds=epoll_wait(epfd,events,20,1000);
     	for(int i=0;i<nfds;i++)
     	{
     		if(events[i].data.fd==listenfd)
     		{
     			clilen=sizeof(cli_addr);
     			new_fd=accept(listenfd,(struct sockaddr *) &cli_addr, &clilen);
-    			accept_event(ev,events,new_fd,epfd);
-    			printf("accepted %d\n",new_fd-4);
+    			if(new_fd<0)
+    				error("accept fail");
+    			ev.data.fd=new_fd;
+    			ev.events=EPOLLIN| EPOLLONESHOT;
+    			epoll_ctl(epfd,EPOLL_CTL_ADD,new_fd,&ev);
+    			printf("accepted Socket ID: %d\n",new_fd);
+    			for (int a=0;a<10;a++)
+    			{
+    				if(fd_list[a]== -1 )
+    				{
+    					fd_list[a]=new_fd;
+    					break;
+    				}
+    			}
     		}
-    		else if(events[i].events==EPOLLIN)
+    		else if(events[i].data.fd>0)
     		{
     		    int conn_fd;
-    			if((conn_fd=events[i].data.fd)<0)
-    				continue ;
-    			for(int a=1;a<(thr_num+1);a++)
+    			conn_fd=events[i].data.fd;
+    			for(int a=0;a<THR_NUM;a++)
     			{
-    				if(thread_ck[a]==true)
+    				if(THREAD_INFO[a].thr_ck==true)
     				{
-    					thread_fd[a]=conn_fd;
+    					THREAD_INFO[a].thr_fd=conn_fd;
     					sem_post(&sem_id[a]);
-    					sleep(0.1);
+    					THREAD_INFO[a].thr_ck=false;
     					break;
     				}
     			}
